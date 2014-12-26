2ba6f44b0f612e7e6c1fe935bc924ad2ff58b793
Alex <adegroat917@gmail.com>
2014-12-26 13:53:30 -0700

    Finish copying feature

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,17 @@ const char* TITLE = "Clipboard+";
 const int WIDTH = 400, HEIGHT = 200;
 bool running = false;
 
+LPTSTR clipboardData[10] = {0};
+
 LRESULT CALLBACK WindProc(HWND, UINT, WPARAM, LPARAM);
 
 long timeMs() {
 	return ((float)clock() / CLOCKS_PER_SEC) * 1000;
+}
+
+void stop() {
+	running = false;
+	PostQuitMessage(0);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -55,22 +62,51 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	MSG message;
 	running = true;
 
+	HGLOBAL hGlobal;
+	LPTSTR temp;
+
 	float startTime = timeMs();
 
 	while(running) {
 
-
 		if(timeMs() - startTime > 150) {
+
 
 			if(GetAsyncKeyState(VK_CONTROL) && GetAsyncKeyState(0x43)) {
 
 				for(int i = 0x30; i < 0x39; i++) {
+					int index = i - 0x30;
 					if(GetAsyncKeyState(i)) {
-						std::cout << "Ctrl+C+" << i - 0x30 << std::endl;
+						//std::cout << "Ctrl+C+" << i - 0x30 << std::endl;
+
+						if(!OpenClipboard(hwnd)) {
+							MessageBox(hwnd, "Error opening clipboard!", "Error", MB_OK);
+							stop();
+						}
+
+						hGlobal = GetClipboardData(CF_TEXT);
+						if(hGlobal) {
+							temp = (LPTSTR)GlobalLock(hGlobal);
+
+							if(temp == NULL) {
+								MessageBox(hwnd, "Error copying data to CB+", "Error", MB_OK);
+							} else {
+								clipboardData[index] = temp;
+							}
+
+							GlobalUnlock(hGlobal);
+						} else {
+							MessageBox(hwnd, "Couldn't copy data", "Error", MB_OK);
+						}
+
+						EmptyClipboard();
+						CloseClipboard();
+						MessageBox(hwnd, clipboardData[index], "Clipboard+", MB_OK);
 					}
 				}
 
 			}
+
 
 			startTime = timeMs();
 		}
@@ -89,8 +125,7 @@ LRESULT CALLBACK WindProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 	switch(message) {
 
 	case WM_DESTROY:
-		running = false;
-		PostQuitMessage(0);
+		stop();
 		break;
 
 	}
